a121bf0d6c4eeb2b81dfdbd52ecb10e9059942c2
LinhNT <LinhNT@DESKTOP-3F7UD16>
2021-12-11 10:33:14 +0700

    Add project files.

---
+++ TestWAD/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWAD.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Fines { get; set; }
+        public int NumFines { get; set; }
+        public DateTime DateFine { get; set; }
+    }
+}
---
+++ TestWAD/Models/ViewModel/StudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestWAD.Models.ViewModel
+{
+    public class StudentViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter form of payment of fines")]
+        public string Fines { get; set; }
+        [Required(ErrorMessage = "Please enter form of payment of fines")]
+        public int NumFines { get; set; }
+        [Required(ErrorMessage = "Please enter form of payment of fines")]
+        public DateTime DateFine { get; set; }
+    }
+}
