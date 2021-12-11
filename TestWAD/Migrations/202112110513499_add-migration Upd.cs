1f833ee8d1b2a1e2b2b78efb8ff476d77dd5e825
Nguyễn Thị Linh <libh1004@gmail.com>
2021-12-11 12:18:45 +0700

    Update

---
+++ TestWAD/Migrations/202112110513499_add-migration Upd.cs
@@ -0,0 +1,16 @@
+namespace TestWAD.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmigrationUpd : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
